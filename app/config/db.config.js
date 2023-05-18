0b2d6d88066e353abdbc45ddca5534a06cd4a2b6
isaacngabwali <31367648+isaacngabwali@users.noreply.github.com>
2023-05-18 09:00:38 +0300

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "123",
-  DB: "testdb",
+  USER: "fanitetest",
+  PASSWORD: "fanitetest",
+  DB: "fanitetest",
   dialect: "postgres",
   pool: {
     max: 5,
